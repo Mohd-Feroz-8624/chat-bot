63a4168e5b4c11e0e3fe6cff60d885129e7b24a1
Feroz <mohamedferoz2023@gmail.com>
2025-11-08 11:04:46 +0530

    set up chat message format

--- src/chatbot-components/ChatMessage.jsx
+++ src/chatbot-components/ChatMessage.jsx
@@ -1,23 +1,12 @@
-import RobotProfileImage from '../assets/robot.png';
-import UserProfileImage from '../assets/user.png';
+import RobotProfileImage from "../assets/robot.png";
+import UserProfileImage from "../assets/user.png";
 
 export function ChatMessage({ message, sender }) {
-
   return (
-
     <div className={sender}>
-      {sender === 'robot' && (
-        <img src={RobotProfileImage} width="40px" />)
-      }
-      <div className="chat-message">
-        {message}
-      </div>
-      {
-        sender === 'user' && (
-          <img src={UserProfileImage} width="40px" />)
-      }
-
+      {sender === "robot" && <img src={RobotProfileImage} width="40px" />}
+      <div className="chat-message">{message}</div>
+      {sender === "user" && <img src={UserProfileImage} width="40px" />}
     </div>
   );
-
 }
